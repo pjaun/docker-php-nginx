a5624a6c020e9f0c78763bc1c9957d0024b9418b
PjAun <pjaun@hotmail.com>
2020-01-16 09:58:22 +0700

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,2 +1,5 @@
 <?php
-phpinfo();
+        echo("<p align=center>");
+        echo("<H1>Welcome..</H1>");
+        echo("<br><br>Docker image with Nginx 1.16 & PHP-FPM 7.3 on Alpine Linux</p>");
+?>
